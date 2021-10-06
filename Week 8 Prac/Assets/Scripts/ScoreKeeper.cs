0c6827e90b43bf83ec4f37b3995a4b1d5a8fd9e6
Sebastiony-Uni <sebastian.westburyprice@students.mq.edu.au>
2021-10-06 12:52:22 +1100

    Updated scripts

--- Week 8 Prac/Assets/Scripts/ScoreKeeper.cs
+++ Week 8 Prac/Assets/Scripts/ScoreKeeper.cs
@@ -8,27 +8,16 @@ public class ScoreKeeper : MonoBehaviour
 
     [HideInInspector]
     public int currentScore;
-
-
-    float time;
-    float every = 5;
     // Start is called before the first frame update
     void Start()
     {
-        time = 0;
         currentScore = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        time += Time.deltaTime;
 
-        if(time >= every)
-        {
-            currentScore+= pickupPoints;
-            time = 0;
-        }
     }
 
     public void pickupCoin()
