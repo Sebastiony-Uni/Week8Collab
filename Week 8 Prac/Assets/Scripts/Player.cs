d9a5361cd2d830c2da419b1c61ee358634dcba78
SaRtHaK-Sa-xena <34013565+SaRtHaK-Sa-xena@users.noreply.github.com>
2021-10-11 20:36:44 +1100

    Coins + Player
    Coins Script created.
    Currently references a ScoreKeeper Obj that does not exist. Will fix itself in the merge to main.

--- Week 8 Prac/Assets/Scripts/Player.cs
+++ Week 8 Prac/Assets/Scripts/Player.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // movement speed
+    public float movementSpeed;
 
     // Update is called once per frame
     void Update()
     {
-        
+        // raw axis
+        float horizontalAxis = Input.GetAxisRaw("Horizontal");
+        float verticalAxis = Input.GetAxisRaw("Vertical");
+
+        // create movement vector
+        Vector3 movement = new Vector3(horizontalAxis, 0, verticalAxis);
+
+        // movement
+        transform.Translate(movement * movementSpeed * Time.fixedDeltaTime);
     }
 }
